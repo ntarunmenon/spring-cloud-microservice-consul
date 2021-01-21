52691a257f6a5c53a60e0fbdd7eef62aaf2182b4
Arun Menon <arun.menon@autogeneral.com.au>
2021-01-21 13:12:58 +1000

    Add keycloak Authentication

--- address-lookup-service/src/main/java/org/arunm/config/AddressServiceSecurityConfig.java
+++ address-lookup-service/src/main/java/org/arunm/config/AddressServiceSecurityConfig.java
@@ -1,8 +1,12 @@
 package org.arunm.config;
 
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
@@ -42,5 +46,31 @@ public class AddressServiceSecurityConfig extends KeycloakWebSecurityConfigurerA
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(final AuthenticationManagerBuilder auth) {
+        final KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+
+		/*
+			Spring Security, when using role-based authentication, requires that role names start
+			with ROLE_. For example, an administrator role must be declared in Keycloak as ROLE_ADMIN
+			or similar, not simply ADMIN.
+
+			The class org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
+			supports an optional org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
+			which can be used to map roles coming from Keycloak to roles recognized by Spring Security.
+			Use, for example, org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
+			to insert the ROLE_ prefix and convert the role name to upper case. The class is part of
+			Spring Security Core module.
+		 */
+        final SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+        simpleAuthorityMapper.setConvertToUpperCase(true);
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 }
--- order-service/src/main/java/org/arunm/OrderServiceApplication.java
+++ order-service/src/main/java/org/arunm/OrderServiceApplication.java
@@ -1,27 +1,30 @@
 package org.arunm;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Random;
+
 import org.arunm.model.Address;
 import org.arunm.model.OrderRequest;
 import org.arunm.model.OrderResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
-import java.util.List;
-import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -40,8 +43,8 @@ public class OrderServiceApplication {
     private RestTemplate restTemplate;
 
     @Bean
-    public RestTemplate loadbalancedRestTemplate() {
-        return new RestTemplate();
+    public RestTemplate loadbalancedRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 
     @PostMapping("/order")
@@ -51,14 +54,12 @@ public class OrderServiceApplication {
                     .path("/api/address")
                     .queryParam("customerId",orderRequest.getCustomerId())
                 .build().toUri();
-        Address address = restTemplate
-                .getForEntity(uri,
-                        Address.class)
-                .getBody();
-        Assert.notNull(address, "Address cannot be null");
-        System.out.println("Address from service is " + address);
-        serviceUrl();
-        orderRequest.setAddress(address);
+
+        HttpEntity<String> entity = new HttpEntity<>(null , addAuthHeaderFromKeycloak());
+
+        ResponseEntity<Address> addressResponseEntity = restTemplate.exchange(uri, HttpMethod.GET,entity, Address.class);
+        Assert.notNull(addressResponseEntity.getBody(), "Address cannot be null");
+        orderRequest.setAddress(addressResponseEntity.getBody());
         return createOrderResponse();
     }
 
@@ -82,5 +83,12 @@ public class OrderServiceApplication {
             return list.get(0).getUri();
         }
         return null;
+    }
+
+    private HttpHeaders addAuthHeaderFromKeycloak() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " +
+                ("eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoS3NPWmhyQTFLaXNOaVNsVlZLNzNrek1DdE45Q25Ya3JCcTdSN3RYMWRBIn0.eyJleHAiOjE2MTEyMDIxODYsImlhdCI6MTYxMTE5ODU4NiwianRpIjoiYmQ4OTA3MzktYjRlZS00N2VjLWJlNDgtZmQzNjM1NzJlNmQ4IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIxYmI5ZWM0Mi04MmY2LTQ4MzUtYjM0OS1lYjY1MTRkYmEwNDEiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZGRyZXNzLWxvb2t1cC1zZXJ2aWNlIiwic2Vzc2lvbl9zdGF0ZSI6IjAzYWFkNzgyLWM0ZTgtNDFkZS1hOTBmLTJiYTdhZDQ1YTI0NSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImNsaWVudEhvc3QiOiIxNzIuMjAuMC4xIiwiY2xpZW50SWQiOiJhZGRyZXNzLWxvb2t1cC1zZXJ2aWNlIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtYWRkcmVzcy1sb29rdXAtc2VydmljZSIsImNsaWVudEFkZHJlc3MiOiIxNzIuMjAuMC4xIn0.Rz7EbdNDiBaC1Zbo8UH61beB3tDWa2YADb9XgJ34vQxcEZ698rBpbCTIQHjoekHE7ZjVHoKLYiIO7TUyAq1B8yo64Jhr7Eq8shN_ZcmhvFscQAFLSGCr3qFzBfXQNGL4H58-_1ONyCqwbyq_nrb2UzvmddUVHmTFo1tIIUCJWeLBJ9JOEftPRUGDMsftzJdTgASD48LieFlHLs-s_Q4PfMOrtO4Jvd7tnJqRrrRKN8fVq2XOep9vObMOtYUj8sVUagVModVUfG6mN2LrkeSh44Wu6WmFM1VTAWpEFHjzvpsCkUCxXFLRwR548G8rxQUOncXm45KxwKMW6WeZWmAU6A"));
+        return headers;
     }
 }
