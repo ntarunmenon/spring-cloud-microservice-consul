41657ce0748287f74fb1b70bd801421b088896b9
Arun Menon <arun.menon@autogeneral.com.au>
2021-01-27 13:57:53 +1000

    Pass along keycloak tokens

--- order-service/src/main/java/org/arunm/OrderServiceApplication.java
+++ order-service/src/main/java/org/arunm/OrderServiceApplication.java
@@ -3,10 +3,13 @@ package org.arunm;
 import java.net.URI;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import org.arunm.model.Address;
 import org.arunm.model.OrderRequest;
 import org.arunm.model.OrderResponse;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.OidcKeycloakAccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +22,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -88,7 +93,18 @@ public class OrderServiceApplication {
     private HttpHeaders addAuthHeaderFromKeycloak() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " +
-                ("eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoS3NPWmhyQTFLaXNOaVNsVlZLNzNrek1DdE45Q25Ya3JCcTdSN3RYMWRBIn0.eyJleHAiOjE2MTEyOTQxNzEsImlhdCI6MTYxMTI5MDU3MSwianRpIjoiZGQ5NmRhNDItZTczMi00MmQ1LWIzMTYtZWRjNTQxOWU5NDQxIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL2F1dGgvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiI0MWQ1ZTJkNi0yZGYzLTQwOTctOTFhMi00OWMzYzQ3MTVhYjUiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJvcmRlci1zZXJ2aWNlIiwic2Vzc2lvbl9zdGF0ZSI6IjhjMzdmMDE1LTY3YzgtNDliZC1hMjAxLThkYzU1MmExNDE3MSIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiY2xpZW50SWQiOiJvcmRlci1zZXJ2aWNlIiwiY2xpZW50SG9zdCI6IjE3Mi4yMC4wLjEiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtb3JkZXItc2VydmljZSIsImNsaWVudEFkZHJlc3MiOiIxNzIuMjAuMC4xIn0.K_CaumFYyA0o9H7PtfcvRC_NMZ_vpMTLhbBorQTxaQyqddlwIEQ2S9BiUNxbq1uw39lx2S1RvBC0H9NQAUOCoYKCMPMObVkeZaFuq7-FdgWNwv06unSdZUoFjkxO9JSk4VxwMbOtQRcbRxI_lNKhRU68uX4J38c79-Co1pxOAWyAawQjToNohb-JIWK_HivyO8d09RfJVzhOeQoRQAhdRGKXAFeibclqwCz_WbDPS0lT7uwQ4I57--3UgDP6frgcKMQiqi5hZUJlrLzjoI6q-O9HUHIrn7fBHBOJasCoc_J_BnFFOFKT-hfFwRvwabXfu1rEahtOqaztM0kRSVcf_A"));
+                getAccessToken());
         return headers;
+    }
+
+    private String getAccessToken() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Stream.of(authentication)
+                .map(Authentication::getDetails)
+                .map(OidcKeycloakAccount.class::cast)
+                .map(OidcKeycloakAccount::getKeycloakSecurityContext)
+                .map(KeycloakSecurityContext::getTokenString)
+                .findFirst()
+                .orElseThrow(IllegalStateException::new);
     }
 }
